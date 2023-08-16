d949090c6baaffd517779ee8fce398a5ed9265c3
kekssinerini <142410290+kekssinerini@users.noreply.github.com>
2023-08-16 14:43:08 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero!"
+
+def main():
+    print("Simple Calculator")
+    print("Operations: +, -, *, /")
+
+    api_key = "5e13f910d092fa2ef9112fa768235cf1c6c7b4e7"
+    num1 = float(input("Enter first number: "))
+    operation = input("Enter operation: ")
+    num2 = float(input("Enter second number: "))
+
+    if operation == '+':
+        result = add(num1, num2)
+    elif operation == '-':
+        result = subtract(num1, num2)
+    elif operation == '*':
+        result = multiply(num1, num2)
+    elif operation == '/':
+        result = divide(num1, num2)
+    else:
+        result = "Invalid operation"
+
+    print("Result:", result)
+
+if __name__ == "__main__":
+    main()
